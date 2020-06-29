25c46595c4a313db190c3a0bc9a9febd0b8703fb
sirbaconsalot <donavynhaley@gmail.com>
2020-06-28 19:36:33 -0700

    Fixed navigation issues for mobile

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const navSlide = ()  => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.naviagtion');
     const navLinks = document.querySelectorAll('.naviagtion li');
+    const html = document.querySelector("html");
 
     burger.addEventListener('click' , () =>{
         // Animate links
@@ -16,9 +17,11 @@ const navSlide = ()  => {
         // Animate bar
         if(nav.style.transform == "translateX(0%)"){
             nav.style.transform = "translateX(100%)";
+            html.style.overflowY = "visible";
         }
         else{
             nav.style.transform = "translateX(0%)";
+            html.style.overflowY = "hidden";
         }
         // Burger Animation
         burger.classList.toggle('toggle');
